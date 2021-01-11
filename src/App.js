ed52d92ea93d030193d93ed13951e81aa76a8fe8
Abraham0912 <09120912ahn@gmail.com>
2021-01-10 20:04:43 -0600

    endFunctionRemoveOrDeleteElement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    characters: [{
+          id: "1",
+          name: 'Charlie',
+          job: 'Janitor',
+          nuevoDato: "NDCharlie"
+        },
+        {
+          id: "2",
+          name: 'Mac',
+          job: 'Bouncer',
+          nuevoDato: "NDMac"
+        },
+        {
+          id: "3",
+          name: 'Dee',
+          job: 'Aspring actress',
+          nuevoDato: "NDDee"
+        },
+        {
+            id: "4",
+            name: 'Dennis',
+            job: 'Bartender',
+            nuevoDato: "NDDennis"
+          },
+          {
+            id: "5",
+            name: 'Abraham',
+            job: 'Developer',
+            nuevoDato: "NDAbraham"
+          }]
+  }
+
+
+  removeCharacter = (index) => {
+    const {characters} = this.state//this.state.characters
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        //return i !== index
+        return character.id !== index;
+
+      }),
+    })
+  }
+
+
+  addCharacter = (index) => {
+    const {characters} = this.state//this.state.characters
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        //return i !== index
+        return character.id !== index;
+
+      }),
+    })
+  }
+
+  render() {
+
+    return (
+        <div className="container">
+            <Table 
+                characterData={this.state.characters} 
+                removeCharacter={this.removeCharacter}
+            />
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
